54d880d6c9d93ebd54f50578868c0e85d14ac612
Denis <msoxrap857@gmail.com>
2025-09-11 11:19:36 +0300

    Добавлена заглушка для регистраций, максимально не полная версия

---
+++ web/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("show_pass").addEventListener("change", function () {
+    const passwordInput = document.getElementById("password");
+    passwordInput.type = this.checked ? "text" : "password";
+});
